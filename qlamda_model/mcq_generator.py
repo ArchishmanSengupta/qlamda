0b9a0e9506300f17f1263954a02f5903083c31bb
Archishman Sengupta <archishman1808@gmail.com>
2024-03-25 22:31:53 +0530

    chore: change code in py standards

--- qlamda_model/mcq_generator.py
+++ qlamda_model/mcq_generator.py
@@ -1,22 +1,27 @@
-import nltk, torch
+import nltk
 from nltk.corpus import wordnet as wn
+
 nltk.download('wordnet')
 
-# Sentences with different meanings but same word "book"
-firstSentence = "Archie would like to read a book on consumer psychology"
-secondSentence = "Archie will book a candle light dinner on monday"
+def find_similar_word_context(original_word):
+    """
+    Finds similar words in different contexts using WordNet.
 
-# Function to find similar sentences with difference word meanings
-# Takes the original word
-# A simple definition is a group of synonym are synsets.
-# Prints the synsets definition, lemma, examples
-def findSimilarWordContext(original_word):
-  
-  allSynsets = wn.synsets(original_word, 'n');
+    Args:
+        original_word (str): The word to find similar contexts for.
 
-  for syns in allSynsets:
-    print (syns, ":", syns.definition())
-    print ('lemmatized :', syns.lemmas())
-    print ('example :', syns.examples(),"\n")
+    Prints:
+        Definitions, lemmas, and examples of different synsets of the word.
+    """
+    all_synsets = wn.synsets(original_word, 'n')
 
-findSimilarWordContext('book')
+    for syns in all_synsets:
+        print(f"{syns}: {syns.definition()}")
+        print(f'lemmatized: {syns.lemmas()}')
+        print(f'example: {syns.examples()}\n')
+
+# Example Sentences with different meanings but the same word "book"
+first_sentence = "Archie would like to read a book on consumer psychology"
+second_sentence = "Archie will book a candlelight dinner on Monday"
+
+find_similar_word_context('book')
