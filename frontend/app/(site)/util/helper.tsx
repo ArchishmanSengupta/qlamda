9d6d959654ec515b639585994617f3f4a3a8b927
Archishman Sengupta <archishman1808@gmail.com>
2024-03-27 23:46:25 +0530

    fix: fix data fetch

--- frontend/app/(site)/util/helper.tsx
+++ frontend/app/(site)/util/helper.tsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export default async function  getData(){
+async function fetchData() {
+  try {
     const response = await axios.get('https://bf25-2405-201-9002-d04b-8a8-3c02-fc8e-ab75.ngrok-free.app/questions');
     const data = response.data;
-    return data;
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
+
+fetchData();
